1e4506349200e6f1e4c1e93ccf2b9806e758a371
Sheyne Anderson <sheyne@sheyne.com>
2020-12-06 09:35:28 +0000

    Solve part 2

--- src/day6.rs
+++ src/day6.rs
@@ -8,7 +8,7 @@ fn input_generator(input: &str) -> Vec<Vec<String>> {
         .collect()
 }
 
-fn count_yeses(input: &[String]) -> u32 {
+fn count_yeses_to_any(input: &[String]) -> u32 {
     input
         .iter()
         .flat_map(|x| x.as_bytes().iter().map(|x| 1u32 << (x - b'a')))
@@ -16,9 +16,27 @@ fn count_yeses(input: &[String]) -> u32 {
         .count_ones()
 }
 
+fn count_yeses_from_all(input: &[String]) -> u32 {
+    input
+        .iter()
+        .map(|x| {
+            x.as_bytes()
+                .iter()
+                .map(|x| 1u32 << (x - b'a'))
+                .fold(0, |acc, x| acc | x)
+        })
+        .fold(0xffffffff, |acc, x| acc & x)
+        .count_ones()
+}
+
 #[aoc(day6, part1)]
 fn solve_part1(input: &[Vec<String>]) -> u32 {
-    input.iter().map(|x| count_yeses(x)).sum()
+    input.iter().map(|x| count_yeses_to_any(x)).sum()
+}
+
+#[aoc(day6, part2)]
+fn solve_part2(input: &[Vec<String>]) -> u32 {
+    input.iter().map(|x| count_yeses_from_all(x)).sum()
 }
 
 #[cfg(test)]
@@ -27,23 +45,23 @@ mod tests {
 
     #[test]
     fn part1_example1() {
-        assert_eq!(count_yeses(&["abc".to_owned()]), 3);
+        assert_eq!(count_yeses_to_any(&["abc".to_owned()]), 3);
     }
     #[test]
     fn part1_example2() {
         assert_eq!(
-            count_yeses(&["a".to_owned(), "b".to_owned(), "c".to_owned()]),
+            count_yeses_to_any(&["a".to_owned(), "b".to_owned(), "c".to_owned()]),
             3
         );
     }
     #[test]
     fn part1_example3() {
-        assert_eq!(count_yeses(&["ab".to_owned(), "ac".to_owned()]), 3);
+        assert_eq!(count_yeses_to_any(&["ab".to_owned(), "ac".to_owned()]), 3);
     }
     #[test]
     fn part1_example4() {
         assert_eq!(
-            count_yeses(&[
+            count_yeses_to_any(&[
                 "a".to_owned(),
                 "a".to_owned(),
                 "a".to_owned(),
@@ -54,6 +72,37 @@ mod tests {
     }
     #[test]
     fn part1_example5() {
-        assert_eq!(count_yeses(&["b".to_owned()]), 1);
+        assert_eq!(count_yeses_from_all(&["b".to_owned()]), 1);
+    }
+    #[test]
+    fn part2_example1() {
+        assert_eq!(count_yeses_from_all(&["abc".to_owned()]), 3);
+    }
+    #[test]
+    fn part2_example2() {
+        assert_eq!(
+            count_yeses_from_all(&["a".to_owned(), "b".to_owned(), "c".to_owned()]),
+            0
+        );
+    }
+    #[test]
+    fn part2_example3() {
+        assert_eq!(count_yeses_from_all(&["ab".to_owned(), "ac".to_owned()]), 1);
+    }
+    #[test]
+    fn part2_example4() {
+        assert_eq!(
+            count_yeses_from_all(&[
+                "a".to_owned(),
+                "a".to_owned(),
+                "a".to_owned(),
+                "a".to_owned()
+            ]),
+            1
+        );
+    }
+    #[test]
+    fn part2_example5() {
+        assert_eq!(count_yeses_from_all(&["b".to_owned()]), 1);
     }
 }
